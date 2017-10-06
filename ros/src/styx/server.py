bc48682b5ade91823db669d7069ad73f599179ba
Shyam Jagannathan <shyam.jagannathan@gmail.com>
2017-10-06 07:56:42 +0530

    First candidate for submission
    - Successful run on simulator
    - Separate SSD detector for site images
    
    Signed-off-by: Shyam Jagannathan <shyam.jagannathan@gmail.com>

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -10,10 +10,10 @@ from bridge import Bridge
 from conf import conf
 
 #Added as simulator car doesn't start even when throttle = 1.0
-eventlet.monkey_patch()
-sio = socketio.Server(async_mode='eventlet')
+#eventlet.monkey_patch()
+#sio = socketio.Server(async_mode='eventlet')
 
-#sio = socketio.Server()
+sio = socketio.Server()
 app = Flask(__name__)
 msgs = []
 
